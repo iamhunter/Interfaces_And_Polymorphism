93aa60560e1cdcce2bf6af9ed16f7a7007b20d94
iams3cond <andr3whunter@gmail.com>
2016-10-22 17:51:59 -0400

    MainDriver fully functioning

--- src/edu/clemson/cpsc/project3/AbstractSSE.java
+++ src/edu/clemson/cpsc/project3/AbstractSSE.java
@@ -65,14 +65,36 @@ public abstract class AbstractSSE implements StringSetExtended {
             if(copiedrhs.contains(tempOrary))
             {
                 result.insert(tempOrary);
-
             }
         }
-
-
     }
     @Override
-    public final void difference(StringSet rhs, StringSet result) {
+    public final void difference(StringSet rhs, StringSet result)
+    {
+        StringSet copiedThis = new StringSet1(100);
+        StringSet copiedrhs = new StringSet1(100);
+        this.copy(copiedThis);
+
+        StringSet tempSet = new StringSet1(100);
+        while(rhs.sizeOfSet() > 0)
+        {
+            String temp = rhs.removeAny();
+            copiedrhs.insert(temp);
+            tempSet.insert(temp);
+        }
+        while(tempSet.sizeOfSet() > 0)
+        {
+            rhs.insert(tempSet.removeAny());
+        }
+
+        while(copiedThis.sizeOfSet() > 0)
+        {
+            String tempOrary = copiedThis.removeAny();
+            if(!copiedrhs.contains(tempOrary))
+            {
+                result.insert(tempOrary);
+            }
+        }
 
 
     }
